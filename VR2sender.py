1c33213a48c2ef17a560a285d835ee5c159469c7
oalakkad <oalakkad@slu.edu>
2022-10-20 17:33:53 -0500

    Changing Send names to VR sender names

---
+++ VR2sender.py
@@ -0,0 +1,51 @@
+import paho.mqtt.client as mqtt
+import time
+from random import randint
+import json
+
+
+
+connected = False
+
+def on_connect(client, userdata, flags, rc):
+    if rc == 0:
+        connected = True
+        print("Connected")
+    else:
+        print("Not Able To Connect")
+
+broker_address = "192.168.1.117"
+user = ""
+password = ""
+name = "VR2"
+
+client = mqtt.Client(name)
+client.on_connect = on_connect
+client.username_pw_set(user,password)
+client.connect(broker_address)
+time.sleep(0.4)
+client.loop_start()
+testnum = 0
+
+def createCommand(ID, command, name):
+    cdict= {
+        "DroneID": ID,
+        "Command": command,
+        "VR_name": name,
+        }
+    return json.dumps(cdict)
+
+
+
+while True:
+    ID = randint(1,4)
+    randCommand = randint(0,5)
+    commandList = ["takeoff", "land", "left", "right", "up", "down"]
+    command = commandList[randCommand]
+    message = createCommand(ID, command, name)
+    client.publish("test", message)
+    print("Publishing... " + f"test{message}")
+
+    time.sleep(6)
+   
+client.loop_stop()

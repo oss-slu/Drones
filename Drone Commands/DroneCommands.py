85efee6399c8c5e1672714dff159a5e579b4e23a
OmarAlAkkad <64267291+OmarAlAkkad@users.noreply.github.com>
2023-04-25 18:46:13 -0500

    updated drone commands with flags and try except

--- Drone Commands/DroneCommands.py
+++ Drone Commands/DroneCommands.py
@@ -10,6 +10,8 @@ import asyncio
 from mavsdk import System
 import json
 
+connect_flag_drone = 0
+takeoff_flag = 0 #0 is landed, 1 is taken off
 
 def on_connect(client,userdata,flags,rc):
     if rc==0:
@@ -32,9 +34,8 @@ def on_message(client, userdata, msg):
 
 def parseMessage(m_decode, droneID):
     #checks id and returns command if matched id
-    for drone in m_decode["DroneID"]:
-        if droneID == drone:
-            execute_command(m_decode["Commands"])
+    if droneID in m_decode["DroneID"]:
+        asyncio.run(execute_command(m_decode["Commands"]))
 
 def create_mqtt_client():
     # create a MQTT client and connect it to the broker here
@@ -63,38 +64,48 @@ def subscribe_to_topics(client):
     # subscribe to all topics here
     client.subscribe("drone")
 
-async def connect_drone():
-    return
-
-
 async def execute_command(command):
+    global connect_flag_drone
+    global takeoff_flag
+    
+    if connect_flag_drone == 0:
+        print('connecting')
+        # Connect to the drone
+        drone = System()
+        
+        await drone.connect(system_address="serial:///dev/serial0:57600")
+        print('connected')
+        
+        connect_flag_drone = 1
 
-    # Connect to the drone
-    drone = System()
-    print('connecting')
-    await drone.connect(system_address="serial:///dev/serial0:57600")
-    print('connected')
+    if command == "TAKEOFF" and takeoff_flag == 0:
+        try:
+            print("arming")
+            await drone.action.arm()
 
-    if command == "TAKEOFF":
-        print("arming")
-        await drone.action.arm()
+            print("take off")
+            await drone.param.set_param_float("MIS_TAKEOFF_ALT", 0.5)
+            await drone.action.takeoff()
+            
+            takeoff_flag = 1
+        except:
+            print("Error: takeoff denied, no GPS lock")
+            
+    if command == "LAND" and takeoff_flag == 1:
+        try:
+            print("landing")
+            await drone.action.land()
 
-        print("take off")
-        await drone.param.set_param_float("MIS_TAKEOFF_ALT", 0.5)
-        await drone.action.takeoff()
+            await drone.action.disarm()
 
-    if command == "LAND":
-        print("landing")
-        await drone.action.land()
-
-        await drone.action.disarm()
-
-
+            takeoff_flag = 0
+        except:
+            print("Error: landing denied, no flight in progress")
+            
 def main():
 
     client = create_mqtt_client() # create and connect to the MQTT broker
     subscribe_to_topics(client)
-    # asyncio.run(connect_drone())
     client.loop_forever()
 
 
